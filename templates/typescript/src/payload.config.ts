0067b31a4ed1b2d0697e762a1f37d181b47f5a2e
Dan Ribbens <dan.ribbens@gmail.com>
2021-01-17 08:22:54 -0500

    feat: removes serverurl from templates to fallback to relative url

--- templates/typescript/src/payload.config.ts
+++ templates/typescript/src/payload.config.ts
@@ -3,7 +3,6 @@ import TodoLists from './collections/TodoLists';
 import Users from './collections/Users';
 
 export default buildConfig({
-  serverURL: 'http://localhost:3000',
   admin: {
     user: Users.slug,
   },
