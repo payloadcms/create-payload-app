88a0cb82a0fddff19fcdbae490c2319550e927d9
Elliot DeNolf <denolfe@gmail.com>
2023-10-13 09:59:02 -0400

    chore: archive and move to monorepo

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  verbose: true,
-  testEnvironment: 'node',
-  testMatch: ['**/src/**/?(*.)+(spec|test|it-test).[tj]s?(x)'],
-  testTimeout: 10000,
-  transform: {
-    '^.+\\.(ts|tsx)?$': 'ts-jest',
-  },
-}
--- src/lib/generate-secret.ts
+++
@@ -1,5 +0,0 @@
-import { randomBytes } from 'crypto'
-
-export async function generateSecret(): Promise<string> {
-  return randomBytes(32).toString('hex').slice(0, 24)
-}
